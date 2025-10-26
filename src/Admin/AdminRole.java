b5eddf19d1ed5ee40b3e4d4dcc1a53e7ed0e1a6c
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-10-27 00:56:33 +0300

    Add files via upload

--- src/Admin/AdminRole.java
+++ src/Admin/AdminRole.java
@@ -19,24 +19,7 @@ public class AdminRole {
     public AdminRole() {
         s = new StudentDatabase("Students.txt");
     }
-public ArrayList<StudentUser> getAllStudents() {
-        s.readFromFile();
-        return s.returnAllRecords();
-    }
-public boolean deleteStudentById(int id) {
-        s.readFromFile();
-        if (!s.containsId(id)) {
-            return false; 
-        }
-        s.deleteRecordId(id);
-        try {
-            s.saveToFile();
-            return true;
-        } catch (IOException e) {
-            System.out.println("Error saving file: " + e.getMessage());
-            return false;
-        }
-    }
+
     public boolean addStudent(int id, String name, int age, String gender, String department, float gpa) {
         s.readFromFile();
         StudentUser student = new StudentUser(id, name, age, gender, department, gpa);
@@ -50,7 +33,7 @@ public boolean deleteStudentById(int id) {
         }
     }
 
-    public boolean updateStudent(int id, String name, int age, String department, String gender, float gpa) {
+    public boolean updateStudent(int id, String name, int age, String gender, String department, float gpa) {
         s.readFromFile();
 
         // Check if student exists
--- src/Admin/AdminUser.java
+++ src/Admin/AdminUser.java
@@ -10,23 +10,10 @@ public class AdminUser implements Representation{
     private String username;
     private String password;
     public AdminUser(String username, String password) {
-        setUsername(username);
-        setPassword(password);
-    }
-    //------------------------------Username & Password Validation---------------------------------------//
-    private void setUsername(String username) {
-        if(username==null || username.trim().isEmpty()){
-            throw new IllegalArgumentException("Error: Username cannot be empty");
-        }
         this.username=username;
-    }
-    //--------------------------------Password Validation--------------------------------------------------//
-    private void setPassword(String password) {
-        if(password==null || password.trim().isEmpty()){
-            throw new IllegalArgumentException("Error: Password cannot be empty");
-        }
         this.password=password;
     }
+
     @Override
     public String lineRepresentation() {
         return String.format("%s,%s",username,password);
